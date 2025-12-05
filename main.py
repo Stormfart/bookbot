4b6385156bc3b171a24b865834ed1239885ac0d3
Stormfart <olivercharlesparker@gmail.com>
2025-12-05 19:10:36 +0100

    latest

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(filepath):
+  with open(filepath) as f:
+    content = f.read()
+    return content
+
+def word_count(text):
+  wc_list = text.split()
+  return len(wc_list)
+
+def main():
+  book = get_book_text("books/frankenstein.txt")
+  wc = word_count(book)
+  print(f"Found {wc} total words")
+
+if __name__ == '__main__':
+  main()
